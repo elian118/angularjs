f3de0cddd839ba3f9dc3af45435296e9752bf467
elian <elian118@naver.com>
2023-07-16 14:44:27 +0900

    add

--- app.js
+++ app.js
@@ -24,10 +24,21 @@
         ];
 
         $scope.remove = (todo) => {
-            console.log(todo);
             const idx = $scope.todos.findIndex((item) => item.id === todo.id);
             console.log(idx);
             idx > -1 && $scope.todos.splice(idx, 1);
+        }
+
+        $scope.add = (newTodoTitle) => {
+            const newTodo = {
+                id: $scope.todos.length ? $scope.todos[$scope.todos.length - 1].id + 1 : 1,
+                title: newTodoTitle,
+                completed: false,
+                createdAt: Date.now()
+            };
+
+            $scope.todos.push(newTodo);
+            $scope.newTodoTitle = ''; // 인풋 초기화
         }
     }]);
 })();
