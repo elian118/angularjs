d259c982e0f4d3149b04788ae1e0c0e88ad012b0
elian <elian118@naver.com>
2023-07-16 16:54:12 +0900

    services 모듈 로컬스토리지 연동

--- services.js
+++ services.js
@@ -1,30 +1,22 @@
 // 서비스
 angular.module('todo').factory('todoStorage', () => {
+    const TODO_DATA = 'TODO_DATA';
     const storage = {
-        todos: [
-            {
-                id: 1,
-                title: '요가수련',
-                completed: false,
-                createdAt: Date.now()
-            },
-            {
-                id: 2,
-                title: '앵귤러 학습',
-                completed: false,
-                createdAt: Date.now()
-            },
-            {
-                id: 3,
-                title: '운동',
-                completed: true,
-                createdAt: Date.now()
-            }
-        ],
-        get: () => storage.todos,
+        todos: [],
+        _saveToLocalStorage: (data) => localStorage.setItem(TODO_DATA, JSON.stringify(data)),
+        _getFromLocalStorage: () => JSON.parse(localStorage.getItem(TODO_DATA || [])),
+        get: () => {
+            // storage.todos = storage._getFromLocalStorage();
+            // 위 할당문 대신 아래 angular.copy() 사용 -> 앵귤러 컴포넌트 라이프사이클에 맞춰야 하기 때문
+            angular.copy(storage._getFromLocalStorage(), storage.todos);
+            return storage.todos;
+        },
         remove: (todo) => {
             const idx = storage.todos.findIndex((item) => item.id === todo.id);
-            idx > -1 && storage.todos.splice(idx, 1);
+            if (idx > -1) {
+                storage.todos.splice(idx, 1);
+                storage._saveToLocalStorage(storage.todos);
+            }
         },
         add: (newTodoTitle) => {
             const newTodo = {
@@ -33,8 +25,8 @@ angular.module('todo').factory('todoStorage', () => {
                 completed: false,
                 createdAt: Date.now()
             };
-
             storage.todos.push(newTodo);
+            storage._saveToLocalStorage(storage.todos);
         }
     };
 
