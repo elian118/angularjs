8647aee76ec23a3ba99aeb7bb708ee220e73b40f
elian <elian118@naver.com>
2023-07-16 16:18:34 +0900

    directive form 분리

--- directives.js
+++ directives.js
@@ -1,6 +1,4 @@
 angular.module('todo').directive('todoTitle', () => ({ template: '<h1 class="text-4xl font-bold mb-4">할 일 목록</h1>' }));
-angular.module('todo').directive('todoItem', function () {
-    return {
-        templateUrl: 'todoItem.tpl.html'
-    }
-});
+angular.module('todo').directive('todoItem', () => ({ templateUrl: 'todoItem.tpl.html' }));
+angular.module('todo').directive('todoFilters', () => ({ templateUrl: 'todoFilters.tpl.html' }));
+angular.module('todo').directive('todoForm', () => ({ templateUrl: 'todoForm.tpl.html' }));
